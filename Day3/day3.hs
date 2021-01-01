f3381bc0d6d796a565417fe85541f7cc8c751342
CrumbsTrace <v.a.kalkman@gmail.com>
2021-01-01 04:41:32 +0100

    Day 3 small fixes

--- Day3/day3.hs
+++ Day3/day3.hs
@@ -7,7 +7,7 @@ main = do
 
 p1 = do 
     xs <- map parseInput . lines <$> readFile "./day3.txt"
-    print $ length $ Map.filter (>1) $ updateMap xs Map.empty
+    print $ length $ Map.filter (>1) $ updateMap xs $ Map.fromList [((x, y), 0) | x <- [0..1000], y <- [0 .. 1000]]
 
 p2 = do
     xs <- map parseInput . lines <$> readFile "./day3.txt"
@@ -22,10 +22,9 @@ overlapping tile = any (overlap tile)
                 = not (min_x >= n_max_x || n_min_x >= max_x) && not (min_y >= n_max_y || n_min_y >= max_y)
 
 updateMap :: [(Int, (Int, Int), (Int, Int))] -> Map.Map (Int, Int) Int -> Map.Map (Int, Int) Int
-updateMap [] occ_map = occ_map
-updateMap ((_, (min_x, min_y), (max_x, max_y)):xs) occ_map = updateMap xs new_map
-    where   new_map = foldl' (\acc (x, y) -> Map.alter (Just . maybe 1 (+1)) (x, y) acc) occ_map coords
-            coords = [(x, y) | x <- [min_x .. max_x -1], y <- [min_y .. max_y -1]]
+updateMap xs occ_map = new_map
+    where   new_map = foldl' (foldl' (\ acc' (x, y) -> Map.adjust (+1) (x, y) acc')) occ_map coords
+            coords = foldl' (\acc (_, (min_x, min_y), (max_x, max_y))-> [(x, y) | x <- [min_x .. max_x -1], y <- [min_y .. max_y -1]]:acc) [] xs
 
 relativeToAbsolutePosition :: [Int] -> (Int, (Int, Int), (Int, Int))
 relativeToAbsolutePosition [elf, x, y, dx, dy] = (elf, (x, y), (dx + x, dy + y))
