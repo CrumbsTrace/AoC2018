e436b3a4290010c4c7904cdf9ae6fe0289e3cb71
CrumbsTrace <v.a.kalkman@gmail.com>
2021-01-01 18:13:02 +0100

    Faster day3 approach

--- Day3/day3.hs
+++ Day3/day3.hs
@@ -1,35 +1,48 @@
 module Main where
-import qualified Data.Map as Map
+import qualified Data.Set as Set
 import Data.List
+type BoundsClaim = (Int, (Int, Int), (Int, Int))
+
 main = do
     p1
     p2 
 
 p1 = do 
     xs <- map parseInput . lines <$> readFile "./day3.txt"
-    print $ length $ Map.filter (>1) $ updateMap xs $ Map.fromList [((x, y), 0) | x <- [0..1000], y <- [0 .. 1000]]
+    print $ length $ getAllOverlapSquares xs
 
 p2 = do
     xs <- map parseInput . lines <$> readFile "./day3.txt"
     print $ getNonOverlapping xs 
 
-getNonOverlapping :: [(Int, (Int, Int), (Int, Int))] -> Int 
+getAllOverlapSquares :: [BoundsClaim] -> Set.Set (Int, Int)
+getAllOverlapSquares xs = foldr (\(c1, c2) acc -> if overlap c1 c2 then Set.union acc $ getOverlapRectangle c1 c2 else acc) Set.empty claimPairs
+    where claimPairs = [(x, y) | (x:ys) <- tails xs, y <- ys]
+
+getNonOverlapping :: [BoundsClaim] -> Int 
 getNonOverlapping (x@(id, min_c, max_c):xs) = if overlapping x xs then getNonOverlapping (xs ++ [x]) else id 
 
-overlapping :: (Int, (Int, Int), (Int, Int)) -> [(Int, (Int, Int), (Int, Int))] -> Bool 
+overlapping :: BoundsClaim -> [BoundsClaim] -> Bool 
 overlapping tile = any (overlap tile)
     where overlap (_, (min_x, min_y), (max_x, max_y)) (_, (n_min_x, n_min_y), (n_max_x, n_max_y))
                 = not (min_x >= n_max_x || n_min_x >= max_x) && not (min_y >= n_max_y || n_min_y >= max_y)
 
-updateMap :: [(Int, (Int, Int), (Int, Int))] -> Map.Map (Int, Int) Int -> Map.Map (Int, Int) Int
-updateMap xs occ_map = new_map
-    where   new_map = foldl' (foldl' (\ acc' (x, y) -> Map.adjust (+1) (x, y) acc')) occ_map coords
-            coords = foldl' (\acc (_, (min_x, min_y), (max_x, max_y))-> [(x, y) | x <- [min_x .. max_x -1], y <- [min_y .. max_y -1]]:acc) [] xs
+overlap :: BoundsClaim -> BoundsClaim -> Bool
+overlap (_, (min_x, min_y), (max_x, max_y)) (_, (n_min_x, n_min_y), (n_max_x, n_max_y))
+                = not (min_x >= n_max_x || n_min_x >= max_x) && not (min_y >= n_max_y || n_min_y >= max_y)
 
-relativeToAbsolutePosition :: [Int] -> (Int, (Int, Int), (Int, Int))
+getOverlapRectangle :: BoundsClaim -> BoundsClaim -> Set.Set (Int, Int)
+getOverlapRectangle (_, (min_x, min_y), (max_x, max_y)) (_, (min_x2, min_y2), (max_x2, max_y2)) = 
+    Set.fromList $ [(x, y) | x <- [o_x_min..o_x_max - 1], y <- [o_y_min..o_y_max - 1]]
+    where   o_x_max = min max_x max_x2
+            o_y_max = min max_y max_y2
+            o_x_min = max min_x min_x2
+            o_y_min = max min_y min_y2
+
+relativeToAbsolutePosition :: [Int] -> BoundsClaim
 relativeToAbsolutePosition [elf, x, y, dx, dy] = (elf, (x, y), (dx + x, dy + y))
 
-parseInput :: [Char] -> (Int, (Int, Int), (Int, Int))
+parseInput :: [Char] -> BoundsClaim
 parseInput xs = relativeToAbsolutePosition $ map read $ splitOn (`elem` ['#', ' ', '@', ':', 'x' , ',']) xs
 
 splitOn     :: (Char -> Bool) -> String -> [String]
